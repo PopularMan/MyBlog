ac35740b9611b2f850931293b74685f515f97783
cczhang <15238067632@163.com>
2017-12-07 17:03:15 +0800

    捕捉异常

--- src/main/java/com/ssm/shiro/ShiroRealmUtil.java
+++ src/main/java/com/ssm/shiro/ShiroRealmUtil.java
@@ -1,64 +1,43 @@
 package com.ssm.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import com.ssm.dto.Admin;
+import com.ssm.service.IAdminService;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.Md5Hash;
-import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.ssm.dao.IAdminDao;
-import com.ssm.dao.IUserDao;
-import com.ssm.dto.ActiveUser;
-import com.ssm.dto.Admin;
-import com.ssm.dto.User;
-import com.ssm.service.IAdminService;
 public class ShiroRealmUtil extends AuthorizingRealm{
 	@Autowired
 	private IAdminService adminService;
 
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(); 
-		
-		Object principal = principalCollection.getPrimaryPrincipal();//获取登录的用户名
-		
-		System.out.println("用户名"+principal);
-		 if("123456".equals(principal)){               //两个if根据判断赋予登录用户权限
-	            info.addRole("admin");
-	            
-	            System.out.println("----");
-	     }
-	     if("user".equals(principal)){
-	            info.addRole("list");
-	     }
-	     System.out.println("------000-");
-	     info.addRole("user");
-	        
-	     return info;
+		System.out.println("授权方法");
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+
+		Admin admin = (Admin)principalCollection.getPrimaryPrincipal();//获取登录的用户名
+		System.out.println(admin.getName()+"22222");
+
+		info.addStringPermission("user:addBlo");
+		return info;
 	}
-	
+
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken arg) throws AuthenticationException {
-		 //1. token 中获取登录的 username! 注意不需要获取password.
-        
+		//1. token 中获取登录的 username! 注意不需要获取password.
+
 		UsernamePasswordToken token=(UsernamePasswordToken) arg;
 		Object principal = token.getPrincipal();
-        
-    	
-        Admin admin=null;
-        
-    	// 第二步：根据用户输入的userCode从数据库查询
+
+
+		Admin admin=null;
+
+		// 第二步：根据用户输入的userCode从数据库查询
 		try {
 			admin = adminService.selectAdmin(principal.toString());
 		} catch (Exception e1) {
@@ -71,44 +50,44 @@ public class ShiroRealmUtil extends AuthorizingRealm{
 			return null;
 		}
 		//数据库查询到的密码
-	    String password=admin.getPass();
-    	//3.获取盐值 
-        String salt = admin.getSalt();
-        
-    
-        String realmName = getName();
-        //返回值实例化
-        SimpleAuthenticationInfo info = 
-                    new SimpleAuthenticationInfo(principal, password, ByteSource.Util.bytes(salt),realmName);
-    	
-    	
-            return info;
+		String password=admin.getPass();
+		//3.获取盐值
+		String salt = admin.getSalt();
+
+
+		String realmName = getName();
+		//返回值实例化
+		SimpleAuthenticationInfo info =
+				new SimpleAuthenticationInfo(admin, password, ByteSource.Util.bytes(salt),realmName);
+
+
+		return info;
 
 	}
 
-    public static void main(String[] args) {
-    	
-    	 //模拟用户输入的密码
-        String source="123456";
+	public static void main(String[] args) {
 
-        //加入我们的盐salt
-        String salt="zcc";
+		//模拟用户输入的密码
+		String source="123456";
 
-        //密码11111经过散列1次得到的密码:f3694f162729b7d0254c6e40260bf15c
-        int hashIterations=1;
+		//加入我们的盐salt
+		String salt="zcc";
+
+		//密码11111经过散列1次得到的密码:f3694f162729b7d0254c6e40260bf15c
+		int hashIterations=1;
 
 
-        //构造方法中：
-        //第一个参数：明文，原始密码
-        //第二个参数：盐，通过使用随机数
-        //第三个参数：散列的次数，比如散列两次，相当 于md5(md5(''))
-        Md5Hash md5Hash=new Md5Hash(source,salt,hashIterations);
+		//构造方法中：
+		//第一个参数：明文，原始密码
+		//第二个参数：盐，通过使用随机数
+		//第三个参数：散列的次数，比如散列两次，相当 于md5(md5(''))
+		Md5Hash md5Hash=new Md5Hash(source,salt,hashIterations);
 
-        String password_md5=md5Hash.toString();
+		String password_md5=md5Hash.toString();
 
-        System.out.println(password_md5);
-        
-       // System.out.println(new  Md5Hash("123456", "abcdefg",10).toString());
-    }
+		System.out.println(password_md5);
+
+		// System.out.println(new  Md5Hash("123456", "abcdefg",10).toString());
+	}
 
 }
